3c996686844f471b17d6c07217c954969fc680b2
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-12 21:11:27 +0800

    Optimize dalvik heap config for performance
    Change-Id: Ifb14a12002e61e0be67896c0f0d505e201cdf958
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- build/phone-xhdpi-6144-dalvik-heap.mk
+++ build/phone-xhdpi-6144-dalvik-heap.mk
@@ -18,8 +18,8 @@
 
 PRODUCT_VENDOR_PROPERTIES += \
     dalvik.vm.heapstartsize?=16m \
-    dalvik.vm.heapgrowthlimit?=256m \
+    dalvik.vm.heapgrowthlimit?=384m \
     dalvik.vm.heapsize?=512m \
-    dalvik.vm.heaptargetutilization?=0.5 \
-    dalvik.vm.heapminfree?=8m \
-    dalvik.vm.heapmaxfree?=32m
+    dalvik.vm.heaptargetutilization?=0.75 \
+    dalvik.vm.heapminfree?=512k \
+    dalvik.vm.heapmaxfree?=8m
