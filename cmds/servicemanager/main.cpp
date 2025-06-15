8412cf8460acd92228b708706506130adff27b34
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-06-16 05:50:33 +0800

    Revert "servicemanager: increase priority to RT(98)"
    not needed
    
    This reverts commit 0b17e30d6fa3b7fb7186ba52ab9c45b403770e1f.
    
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- cmds/servicemanager/main.cpp
+++ cmds/servicemanager/main.cpp
@@ -158,10 +158,14 @@ int main(int argc, char** argv) {
 
     LOG(INFO) << "Starting sm instance on " << driver;
 
-    struct sched_param sm_param = { .sched_priority = 98 };
+    int sched_policy = SCHED_FIFO;
+    int newPriority = sched_get_priority_min(sched_policy);
 
-    if (sched_setscheduler(0, SCHED_FIFO, &sm_param)) {
-       LOG(ERROR) << "Failed to set ServiceManager priority to RT(98)";
+    struct sched_param param;
+    param.sched_priority = newPriority;
+
+    if (sched_setscheduler(0, sched_policy, &param)) {
+       LOG(ERROR) << "Failed to set ServiceManager priority to SCHED_FIFO";
     }
 
     sp<ProcessState> ps = ProcessState::initWithDriver(driver);
